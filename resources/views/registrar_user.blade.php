4a95f2806dffcb339eb61f93c338684061eb51d5
Gustavo <gwgustavowagner@hotmail.com>
2025-08-06 12:06:09 -0300

    adciona formulário de registro

---
+++ resources/views/registrar_user.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <label>Email</label>
+    <input type="text" placeholder="Insira o email">
+    <label>Senha</label>
+    <input type="password" placeholder="Insira a senha aqui">
+</div>
