5f4b7a3147296e45ba6520782b731c9d9c170a68
Gustavo <gwgustavowagner@hotmail.com>
2025-08-07 14:00:03 -0300

    feat: update routes to include user authentication and dashboard access

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,22 @@
 <?php
 
-use App\Http\Controllers\CadastroController;
+use App\Http\Controllers\{CadastroController, AuthController, DashboardController};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/index', [CadastroController::class, 'index'])->name('usuario.index');
 Route::get('/cadastro', [CadastroController::class, 'create'])->name('usuario.create');
+Route::post('/store', [CadastroController::class, 'store'])->name('usuario.store');
+
+// login
+
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/autenticar', [AuthController::class, 'autenticar'])->name('login.autenticar');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+
+//logout
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
