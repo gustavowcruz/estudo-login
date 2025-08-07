668f47125d1d74ced74d7fe795fc3c6934714b30
Gustavo <gwgustavowagner@hotmail.com>
2025-08-07 13:59:52 -0300

    feat: create dashboard, index, login, and registration views with user authentication

--- resources/views/registrar_user.blade.php
+++ resources/views/registrar_user.blade.php
@@ -1,37 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/app-CCFCCQz7.css') }}">
-    <title>Registro</title>
-</head>
-<body>
-    <div class="w-100%">
-    <form>
+@extends('layout.acesso')
+@section('content')
+<div class='m-10'>
+    <form action="{{ route('usuario.store') }}" method="POST" class="w-full max-w-sm mx-auto">
         @csrf
         @method('POST')
-
         <h1 class='titulo text-2xl font-bold text-center mb-10'>Tela de cadastro minimalista</h1>
-        <div class="w-100% grid grid-cols-2 gap-1">
-            <div class=''>
-                <label>Nome</label>
-                <input type="text" placeholder="Insira o nome" name="nome">
+        <div class="items-center justify-center flex flex-col gap-4 allign-center">
+            <div class='rounded'>
+                <label class=" ">Nome</label>
+                <input type="text" placeholder="Insira o nome" name="nome" class="bg-gray-200">
             </div>
-            <div class=''>
-                <label>Email</label>
-                <input type="text" placeholder="Insira o email" name="email">
+            <div class='rounded'>
+                <label class="">Email</label>
+                <input type="text" placeholder="Insira o email" name="email" class="bg-gray-200">
             </div>
-            <div>
-                <label>Senha</label>
-                <input type="password" placeholder="Insira a senha aqui" name="senha">
+            <div class='rounded'>
+                <label class="">Senha</label>
+                <input type="password" placeholder="Insira a senha aqui" name="senha" class="bg-gray-200">
             </div>
         </div>
-        <div class='items-center text-center'><button class="bg-blue-500 text-white py-2 px-4 rounded cursor-pointer shadow-xl" type="submit">Cadastrar</button></div>
+        <div class='items-center text-center'><button class="bg-blue-500 text-white py-2 px-4 rounded cursor-pointer shadow-xl mt-4" type="submit">Cadastrar</button></div>
     </form>
 </div>
 </body>
 </html>
+@endsection
