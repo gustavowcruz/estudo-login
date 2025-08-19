252fb741f732d705178bfed36ff8d3a416f9cb42
Gustavo <gwgustavowagner@hotmail.com>
2025-08-19 13:54:54 -0300

    feat: update PerfilController to enhance user profile editing functionality

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\EmailVerification;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class PerfilController extends Controller
 {
@@ -13,12 +17,26 @@ class PerfilController extends Controller
 
     public function edit()
     {
-        return view('editar_perfil');
+        $usuario = User::findOrFail(Auth::id());
+        return view('users.alterar-dados', compact('usuario'));
     }
 
     public function update(Request $request)
     {
-        // Lógica para atualizar o perfil do usuário
-        
+        $usuario = User::findOrFail(Auth::id());
+        $request->validate([
+            'nome' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'senha' => 'nullable|string|min:8|confirmed',
+        ]);
+
+        $usuario->name = $request->input('nome');
+        $usuario->email = $request->input('email');
+
+        if ($request->filled('senha')) {
+            $usuario->password = bcrypt($request->input('senha'));
+        }
+
+        return redirect()->route('perfil.index')->with('success', 'Perfil atualizado com sucesso!');
     }
 }
